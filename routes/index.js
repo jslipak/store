538710c49d554fb2d668457951c9894381c24ba6
Jonathan Slipak <seriedigital@gmail.com>
2017-03-13 23:11:49 -0300

    correccion sigue fallando

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var passport = require ("passport")
-var User = require ("../models/user")
+var passport = require("passport");
+var User = require("../models/user");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,7 +15,7 @@ res.render('register')
 
 router.post('/register', function(req,res){
 console.log(req.body.username)
-var newuser = new User ({username: req.body.username})
+var newuser = new User({username: req.body.username})
 User.register(newuser, req.body.password, function (err, user) {
    if (err) {
      console.log(err);
