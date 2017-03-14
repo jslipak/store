ed7f57b222ef3a445cd637e94b9b8f6a577e1e34
Jonathan Slipak <seriedigital@gmail.com>
2017-03-14 10:58:31 -0300

    modif user

--- models/user.js
+++ models/user.js
@@ -3,4 +3,4 @@ var passportLocalMongoose = require("passport-local-mongoose")
 var Schema = mongoose.Schema
 var userSchema = new Schema({})
 userSchema.plugin(passportLocalMongoose)
-module.export= mongoose.model('User', userSchema)
+module.exports= mongoose.model('User', userSchema)
