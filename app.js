76f140de9a2204eff7bf039de83d140a68b955ac
Jonathan Slipak <seriedigital@gmail.com>
2017-03-27 13:38:33 -0300

    no me anda el favicon

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ passport.deserializeUser(Users.deserializeUser()); // Función que recupera los 
       next()
     })
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public','images','favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
