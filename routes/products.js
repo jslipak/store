dec1f426f1db680c4b98753e57f8fcac671ff5ed
Jonathan Slipak <seriedigital@gmail.com>
2017-03-28 18:13:26 -0300

    cambios product

--- routes/products.js
+++ routes/products.js
@@ -11,9 +11,32 @@ router.get('/new',function(req,res){
   res.render("addItem")
 });
 
-router.post('/', function(req,res){
+router.post('/', middleware.isLoggedIn,function(req,res){
   console.log(req.body)
   res.render('index')
 } )
+
+app.post("/", middleware.isLoggedIn, function(req, res){
+  var product=req.body.nameProduct
+  var author={
+    id:req.user._id,
+    username: req.user.username
+  }
+  product.author=author
+  product.descriptionProduct=reg.body.descriptionProduct
+  product.urlImageProduct=req.body.urlImageProduct
+  product.price=req.body.price
+  Product.create(product, function(err, doc){
+    if(err){
+      console.log(err);
+      // res.render("error")
+      res.sendStatus(500);
+    }else{
+      //res.redirect("/products")
+      res.sendStatus(200);
+    }
+  })
+
+})
 
 module.exports = router;
